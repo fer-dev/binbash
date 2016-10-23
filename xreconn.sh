3bb8798bb69a42084264f3cda58d2259a6173d7a
fer-dev <the.crazy.shadows@outlook.es>
2016-10-23 10:16:11 -0500

    Update xreconn.sh

--- xreconn.sh
+++ xreconn.sh
@@ -1,27 +1,3 @@
 
-#!/bin/bash
-
-#Configuracion de  para resolver direcciones ip de forma dinamica
-#FASE 1
-echo "========================================================================"
-#run smbd 
-/etc/init.d/smbd start
-#start network config
-ifdown eth0
-#FASE 2 
-echo "========================================================================"
-#change/spoof mac
-macchanger -r eth0
-macchanger -a eth0 
-#change vendor and mac address
-#FASE 3
-echo "========================================================================"
-#finish config and show ip adress it have to change.
-ifup eth0  | ifconfig | grep inet
-#termina fase 3
-echo "========================================================================" 
-echo "=====================///..Analizando...///=============================="
-exit 16
-
 
 
